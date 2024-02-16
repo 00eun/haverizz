5e62f63d07b4607a0220bd04117d33e4fe317a6a
youngeun <00eun1128@gmail.com>
2024-02-16 22:43:25 +0900

    feat : 테스트글작성

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ export default function Home() {
       <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.description}>안녕하세요</div>
         <div>여기는 HAVE RIZZ 의 공간입니다</div>
+        <div>테스트 글입니다</div>
       </main>
     </>
   );
